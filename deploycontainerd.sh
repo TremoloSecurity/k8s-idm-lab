8d19931a133a31448ef3737022ea6a017cb9a883
Marc Boorshtein <marc@tremolo.io>
2021-08-11 10:26:42 -0400

    cleanup on aisle apt

--- deploycontainerd.sh
+++ deploycontainerd.sh
@@ -32,6 +32,8 @@ echo \
   "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
+rm -rf /etc/containerd
+
 apt-get update
 apt-get -y install  containerd.io
 
