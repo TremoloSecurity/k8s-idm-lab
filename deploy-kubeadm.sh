3175e58b936749bab2d4d6d3491fa4240a5be283
Marc Boorshtein <marc@tremolo.io>
2022-05-17 15:04:42 -0400

    force updates

--- deploy-kubeadm.sh
+++ deploy-kubeadm.sh
@@ -6,5 +6,7 @@ apt-get update && apt-get install -y apt-transport-https ca-certificates curl
 sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
 echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 apt-get update
+apt-get -y upgrade
+apt-get update
 apt-get install -y kubelet kubeadm kubectl
 apt-mark hold kubelet kubeadm kubectl
