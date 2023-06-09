e79a8a1f7d20f762c56fbcf5e928bdb9ff9c6e2a
Marc Boorshtein <marc@tremolo.io>
2023-06-09 09:30:42 -0400

    fix ubuntu packages

--- deploy-kubeadm.sh
+++ deploy-kubeadm.sh
@@ -1,10 +1,10 @@
-apt-get update && apt-get install -y apt-transport-https ca-certificates curl
+apt-get update && apt-get install -y apt-transport-https ca-certificates curl gnupg gnupg2
 #curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 #cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
 #deb https://apt.kubernetes.io/ kubernetes-xenial main
 #EOF
-sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
-echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /etc/apt/trusted.gpg.d/kubernetes-archive-keyring.gpg add
+echo "deb [signed-by=/etc/apt/trusted.gpg.d/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 apt-get update
 apt-get -y upgrade
 apt-get update
