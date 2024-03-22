d2576968cea92559fc9ca6b3a56c2e8541577f16
Marc Boorshtein <marc@tremolo.io>
2024-03-22 11:50:10 +0100

    add keyrings dir

--- deploy-kubeadm.sh
+++ deploy-kubeadm.sh
@@ -3,6 +3,7 @@ apt-get update && apt-get install -y apt-transport-https ca-certificates curl gn
 #cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
 #deb https://apt.kubernetes.io/ kubernetes-xenial main
 #EOF
+mkdir -p -m 755 /etc/apt/keyrings
 curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' |  tee /etc/apt/sources.list.d/kubernetes.list
 apt-get update
