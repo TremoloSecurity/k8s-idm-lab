a84f246fe065c38c7dbdf6edbf499d11f75d1acf
Marc Boorshtein <marc@tremolo.io>
2024-03-22 10:23:03 +0100

    update package managers

--- deploy-kubeadm.sh
+++ deploy-kubeadm.sh
@@ -3,8 +3,8 @@ apt-get update && apt-get install -y apt-transport-https ca-certificates curl gn
 #cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
 #deb https://apt.kubernetes.io/ kubernetes-xenial main
 #EOF
-curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /etc/apt/trusted.gpg.d/kubernetes-archive-keyring.gpg add
-echo "deb [signed-by=/etc/apt/trusted.gpg.d/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' |  tee /etc/apt/sources.list.d/kubernetes.list
 apt-get update
 apt-get -y upgrade
 apt-get update
