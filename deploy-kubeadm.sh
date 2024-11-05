351f660f2b256c756f89a2a6a766d284947e433b
Marc Boorshtein <marc.boorshtein@tremolosecurity.com>
2024-11-05 10:45:54 -0500

    update to 1.31

--- deploy-kubeadm.sh
+++ deploy-kubeadm.sh
@@ -4,8 +4,8 @@ apt-get update && apt-get install -y apt-transport-https ca-certificates curl gn
 #deb https://apt.kubernetes.io/ kubernetes-xenial main
 #EOF
 mkdir -p -m 755 /etc/apt/keyrings
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' |  tee /etc/apt/sources.list.d/kubernetes.list
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.31/deb/Release.key | gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.31/deb/ /' |  tee /etc/apt/sources.list.d/kubernetes.list
 apt-get update
 apt-get -y upgrade
 apt-get update
